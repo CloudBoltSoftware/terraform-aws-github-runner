ccfdfb6bf8663a79b6b87517e0b397388af141bf
EwiththeBowtie <eeinowski@cloudboltsoftware.com>
2020-11-11 22:38:38 -0800

    Add aws install

--- modules/runners/templates/user-data.sh
+++ modules/runners/templates/user-data.sh
@@ -7,6 +7,11 @@ yum update -y
 
 # Install runner
 cd /home/ec2-user || cd /home/centos 
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
 mkdir actions-runner && cd actions-runner
 
 aws s3 cp ${s3_location_runner_distribution} actions-runner.tar.gz
