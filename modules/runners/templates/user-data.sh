100579b58fd6a5c284d51112cd5d0712e7d618dc
EwiththeBowtie <eeinowski@cloudboltsoftware.com>
2020-11-11 23:26:44 -0800

    Install jq

--- modules/runners/templates/user-data.sh
+++ modules/runners/templates/user-data.sh
@@ -4,7 +4,8 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 ${pre_install}
 
 yum update -y
-
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y jq
 # Install runner
 cd /home/ec2-user || cd /home/centos 
 
